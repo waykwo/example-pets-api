8c9f3a0152b1a42c0f6833fc50fb9fa5bf106ef6
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-11 20:50:23 -0400

    Added index route and controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,6 @@
 class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+    render :index
+  end
 end
---
+++ app/views/pets/_pet.json.jbuilder
@@ -0,0 +1,4 @@
+json.id pet.id
+json.name pet.name
+json.age pet.age
+json.breed pet.breed
