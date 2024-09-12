114d72df98eabdd6115029678690efcac09286a2
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-11 20:29:20 -0400

    Add model, route, controller

---
+++ db/migrate/20240912002427_create_pets.rb
@@ -0,0 +1,11 @@
+class CreatePets < ActiveRecord::Migration[7.1]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.integer :age
+      t.string :breed
+
+      t.timestamps
+    end
+  end
+end
